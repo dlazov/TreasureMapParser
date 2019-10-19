978db7a79e6c7cb0b9f0090854ce521292087f12
Don Lazov <zovs66@gmail.com>
2019-10-19 01:46:00 -0400

    Refactored the parseData method further, splitting out methods.

--- src/com/zovssoftware/pirates/ScanAndParse.java
+++ src/com/zovssoftware/pirates/ScanAndParse.java
@@ -22,87 +22,102 @@ public class ScanAndParse {
             String listNormalized = list.get(count).toLowerCase();
             String[] splitString = listNormalized.split(",", 0);
             for (int i = 0; i <= splitString.length; i++) {
-                if (splitString[0].contains("walk")) {
-                    speed = WALK_SPEED;
-                    System.out.println("Speed is now: " + speed);
-                } else if (splitString[0].contains("run")) {
-                    speed = RUN_SPEED;
-                    System.out.println("Speed is now: " + speed);
-                } else if (splitString[0].contains("horse trot")) {
-                    speed = HORSE_TROT;
-                    System.out.println("Speed is now: " + speed);
-                } else if (splitString[0].contains("horse gallop")) {
-                    speed = HORSE_GALLOP;
-                    System.out.println("Speed is now: " + speed);
-                } else if (splitString[0].contains("elephant ride")) {
-                    speed = ELEPHANT_RIDE;
-                    System.out.println("Speed is now: " + speed);
-                } else {
-                    System.out.println("\nYe got th' wrong booty map Matie! Ye used: " + list.get(count).toString());
-                    System.out.println("Try changin' yer booty map in th' followin' ways!");
-                    System.out.println("Mode o' transport: [Walk, Run, Horse trot, Horse gallop or Elephant ride]");
-                }
+                speed = getSpeed(list.get(count), speed, splitString[0]);
                 int time = 0;
                 for (String s : splitString) {
-                    try {
-                        time = Integer.parseInt(splitString[1]);
-                    } catch (NumberFormatException nfe) {
-                        nfe.printStackTrace();
-                    }
-                    int distance = 0;
-                    if (speed == WALK_SPEED) {
-                        int minutes = 20;
-                        distance = speed * time;
-                        rate = minutes / distance;
-                        System.out.println("Miles traveled is: " + rate);
-                    } else if (speed == RUN_SPEED) {
-                        int minutes = 10;
-                        distance = speed * time;
-                        rate = minutes / distance;
-                        System.out.println("Miles traveled is: " + rate);
-                    } else if (speed == HORSE_TROT) {
-                        int minutes = 15;
-                        distance = speed * time;
-                        rate = minutes / distance;
-                        System.out.println("Miles traveled is: " + rate);
-                    } else if (speed == HORSE_GALLOP) {
-                        int minutes = 4;
-                        distance = speed * time;
-                        rate = minutes / distance;
-                        System.out.println("Miles traveled is: " + rate);
-                    } else if (speed == ELEPHANT_RIDE) {
-                        int minutes = 10;
-                        distance = speed * time;
-                        rate = minutes / distance;
-                        System.out.println("Miles traveled is: " + rate);
-                    } else {
-                        System.out.println("\nYe got th' wrong booty map Matie! Ye used: " + list.get(count).toString() + s);
-                        System.out.println("Try changin' yer booty map in th' followin' ways!");
-                        System.out.println("Time o' travel in minutes: [5, 10, 20]");
-                    }
+                    time = getTime(list, count, speed, splitString, time, s);
                 }
+                String direction = "";
                 for (int k = 0; k <= splitString.length; k++) {
-                    String direction = "";
-                    if (splitString[2].contains("n")) {
-                        direction = splitString[2].toUpperCase();
-                        System.out.println("Direction is now: " + direction);
-                    } else if (splitString[2].contains("s")) {
-                        direction = splitString[2].toUpperCase();
-                        System.out.println("Direction is now: " + direction);
-                    } else if (splitString[2].contains("e")) {
-                        direction = splitString[2].toUpperCase();
-                        System.out.println("Direction is now: " + direction);
-                    } else if (splitString[2].contains("w")) {
-                        direction = splitString[2].toUpperCase();
-                        System.out.println("Direction is now: " + direction);
-                    } else {
-                        System.out.println("\nYe got th' wrong booty map Matie! Ye used: " + list.get(count).toString());
-                        System.out.println("Try changin' yer booty map in th' followin' ways!");
-                        System.out.println("Direction o' travel: [N,S,E,W]\n");
-                    }
+                    getDirection(list.get(count), splitString[2]);
                 }
             }
         }
+    }
+
+    private static void getDirection(String s, String s2) {
+        String direction;
+        if (s2.contains("n")) {
+            direction = s2.toUpperCase();
+            System.out.println("Direction is now: " + direction);
+        } else if (s2.contains("s")) {
+            direction = s2.toUpperCase();
+            System.out.println("Direction is now: " + direction);
+        } else if (s2.contains("e")) {
+            direction = s2.toUpperCase();
+            System.out.println("Direction is now: " + direction);
+        } else if (s2.contains("w")) {
+            direction = s2.toUpperCase();
+            System.out.println("Direction is now: " + direction);
+        } else {
+            System.out.println("\nYe got th' wrong booty map Matie! Ye used: " + s.toString());
+            System.out.println("Try changin' yer booty map in th' followin' ways!");
+            System.out.println("Direction o' travel: [N,S,E,W]\n");
+        }
+    }
+
+    private static int getTime(LinkedList<String> list, int count, int speed, String[] splitString, int time, String s) {
+        try {
+            time = Integer.parseInt(splitString[1]);
+        } catch (NumberFormatException nfe) {
+            nfe.printStackTrace();
+        }
+        int distance = 0;
+        if (speed == WALK_SPEED) {
+            int minutes = 20;
+            distance = speed * time;
+            rate = minutes / distance;
+            System.out.println("Miles traveled is: " + rate);
+        } else if (speed == RUN_SPEED) {
+            int minutes = 10;
+            distance = speed * time;
+            rate = minutes / distance;
+            System.out.println("Miles traveled is: " + rate);
+        } else if (speed == HORSE_TROT) {
+            int minutes = 15;
+            distance = speed * time;
+            rate = minutes / distance;
+            System.out.println("Miles traveled is: " + rate);
+        } else if (speed == HORSE_GALLOP) {
+            int minutes = 4;
+            distance = speed * time;
+            rate = minutes / distance;
+            System.out.println("Miles traveled is: " + rate);
+        } else if (speed == ELEPHANT_RIDE) {
+            int minutes = 10;
+            distance = speed * time;
+            rate = minutes / distance;
+            System.out.println("Miles traveled is: " + rate);
+        } else {
+            System.out.println("\nYe got th' wrong booty map Matie! Ye used: " + list.get(count).toString() + s);
+            System.out.println("Try changin' yer booty map in th' followin' ways!");
+            System.out.println("Time o' travel in minutes: [5, 10, 20]");
+        }
+        return time;
+    }
+
+    private static int getSpeed(String s, int speed, String s2) {
+        if (s2.contains("walk")) {
+            speed = WALK_SPEED;
+            System.out.println("Speed is now: " + speed);
+        } else if (s2.contains("run")) {
+            speed = RUN_SPEED;
+            System.out.println("Speed is now: " + speed);
+        } else if (s2.contains("horse trot")) {
+            speed = HORSE_TROT;
+            System.out.println("Speed is now: " + speed);
+        } else if (s2.contains("horse gallop")) {
+            speed = HORSE_GALLOP;
+            System.out.println("Speed is now: " + speed);
+        } else if (s2.contains("elephant ride")) {
+            speed = ELEPHANT_RIDE;
+            System.out.println("Speed is now: " + speed);
+        } else {
+            System.out.println("\nYe got th' wrong booty map Matie! Ye used: " + s.toString());
+            System.out.println("Try changin' yer booty map in th' followin' ways!");
+            System.out.println("Mode o' transport: [Walk, Run, Horse trot, Horse gallop or Elephant ride]");
+        }
+        return speed;
     }
 
     public int getCount() {
@@ -121,15 +136,14 @@ public class ScanAndParse {
         try {
             Scanner sc = new Scanner(new FileInputStream(file));
             sc.useDelimiter(",");
-            while (sc.hasNextLine()){
+            while (sc.hasNextLine()) {
                 content = sc.nextLine();
                 list.add(content);
             }
             sc.close();
-        } catch(FileNotFoundException fnf) {
+        } catch (FileNotFoundException fnf) {
             fnf.printStackTrace();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println("\nTh' booty map was no good, try again");
         }
